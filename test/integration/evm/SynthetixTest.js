d060c224747c8610c3d179b660c631fb2154cb48
0xCooler <0xcooler@gmail.com>
2021-05-18 11:04:10 +1000

    add synthetix integration test

--- test/integration/evm/SynthetixTest.js
+++ test/integration/evm/SynthetixTest.js
@@ -4,27 +4,33 @@ const chaiAlmost = require('chai-almost');
 use(chaiAlmost());
 
 const checkAlmostSame = (a, b) => {
-    expect(ethers.BigNumber.from(a).gt(ethers.BigNumber.from(b).mul(99).div(100))).to.be.true;
-    expect(ethers.BigNumber.from(a).lt(ethers.BigNumber.from(b).mul(101).div(100))).to.be.true;
+    expect(ethers.BigNumber.from(a).gt(ethers.BigNumber.from(b).mul(95).div(100))).to.be.true;
+    expect(ethers.BigNumber.from(a).lt(ethers.BigNumber.from(b).mul(105).div(100))).to.be.true;
 }
 
-const uniswapV2Factory = "0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f";
-const uniswapV2Router = "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D";
-
 // For mainnet
+const susd_price_feed = "0xad35Bd71b9aFE6e4bDc266B345c198eaDEf9Ad94";
+const eth_price_feed = "0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419";
+const link_price_feed = "0x2c1d072e956AFFC0D435Cb7AC38EF18d24d9127c";
+
 const weth = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2";
 const usdc = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48";
-const usdt = "0xdac17f958d2ee523a2206206994597c13d831ec7";
-const eth_price_feed = "0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419";
-const usdt_price_feed = "0x3E7d1eAB13ad0104d2750B8863b489D65364e32D";
-const usdc_price_feed = "0x8fFfFfd4AfB6115b954Bd326cbe7B4BA576818f6";
-let WETH, USDC, USDT, UniswapRouter;
+const susd = "0x57Ab1ec28D129707052df4dF418D58a2D46d5f51";
+const seth = "0x5e74C9036fb86BD7eCdcb084a0673EFc32eA31cb";
+const slink = "0xbBC455cb4F1B9e4bFC4B73970d360c8f032EfEE6";
+const susdKey = '0x7355534400000000000000000000000000000000000000000000000000000000';
+const sethKey = '0x7345544800000000000000000000000000000000000000000000000000000000';
+const slinkKey = '0x734c494e4b000000000000000000000000000000000000000000000000000000';
+const addressResolverAddress = "0x823bE81bbF96BEc0e25CA13170F5AaCb5B79ba83";
+const synthetixAddress = "0x97767D7D04Fd0dB0A1a2478DCd4BA85290556B48";
+const uniswapV2RouterAddress = "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D";
 
-let logicOwner, manager, dao, user;
-let PoolFactory, PoolLogic, PoolManagerLogic, PriceConsumerLogic;
-let poolFactory, poolLogic, poolManagerLogic, priceConsumerLogic, poolLogicProxy, poolManagerLogicProxy, fundAddress;
+describe("Synthetix Test", function() {
+    let WETH, susdProxy, sethProxy, slinkProxy, addressResolver, synthetix, uniswapV2Router;
+    let logicOwner, manager, dao, user;
+    let PoolFactory, PoolLogic, PoolManagerLogic, PriceConsumerLogic;
+    let poolFactory, poolLogic, poolManagerLogic, priceConsumerLogic, poolLogicProxy, poolManagerLogicProxy, fundAddress;
 
-describe("PoolFactory", function() {
     before(async function(){
         [logicOwner, manager, dao, user] = await ethers.getSigners();
 
@@ -58,17 +64,30 @@ describe("PoolFactory", function() {
         await poolFactory.deployed();
 
          // Initialize Asset Price Consumer
-         const assetWeth = { asset: weth, assetType: 0, aggregator: eth_price_feed };
-         const assetUsdt = { asset: usdt, assetType: 0, aggregator: usdt_price_feed };
-         const assetUsdc = { asset: usdc, assetType: 0, aggregator: usdc_price_feed };
-         const priceConsumerInitAssets = [assetWeth, assetUsdt, assetUsdc];
+        const assetSusd = { asset: susd, assetType: 0, aggregator: susd_price_feed };
+        const assetSeth = { asset: seth, assetType: 0, aggregator: eth_price_feed };
+        const assetSlink = { asset: slink, assetType: 0, aggregator: link_price_feed };
+        const priceConsumerInitAssets = [assetSusd, assetSeth, assetSlink];
  
-         await priceConsumer.initialize(poolFactory.address, priceConsumerInitAssets);
-         await priceConsumer.deployed();
+        await priceConsumer.initialize(poolFactory.address, priceConsumerInitAssets);
+        await priceConsumer.deployed();
 
         poolFactory = await PoolFactory.attach(poolFactory.address);
         await poolFactory.initialize(poolLogic.address, poolManagerLogic.address, priceConsumerProxy.address, dao.address);
         await poolFactory.deployed();
+
+        const IAddressResolver = await hre.artifacts.readArtifact("IAddressResolver");
+        addressResolver = await ethers.getContractAt(IAddressResolver.abi, addressResolverAddress);
+
+        const IUniswapV2Router = await hre.artifacts.readArtifact("IUniswapV2Router");
+        uniswapV2Router = await ethers.getContractAt(IUniswapV2Router.abi, uniswapV2RouterAddress);
+
+        const ISynthetix = await hre.artifacts.readArtifact("ISynthetix");
+        synthetix = await ethers.getContractAt(ISynthetix.abi, synthetixAddress);
+
+        const SynthetixGuard = await ethers.getContractFactory('SynthetixGuard');
+        synthetixGuard = await SynthetixGuard.deploy(addressResolver.address);
+        synthetixGuard.deployed();
 
         const ERC20Guard = await ethers.getContractFactory("ERC20Guard");
         erc20Guard = await ERC20Guard.deploy();
@@ -79,22 +98,24 @@ describe("PoolFactory", function() {
         uniswapV2Guard.deployed();
 
         await poolFactory.connect(dao).setERC20Guard(erc20Guard.address);
-        await poolFactory.connect(dao).setGuard(uniswapV2Router, uniswapV2Guard.address);
+        await poolFactory.connect(dao).setGuard(uniswapV2Router.address, uniswapV2Guard.address);
+        await poolFactory.connect(dao).setGuard(synthetix.address, synthetixGuard.address);
     });
 
-    it("Should be able to get WETH", async function() {
+    it("Should be able to get susd", async function() {
         const IWETH = await hre.artifacts.readArtifact("IWETH");
         WETH = await ethers.getContractAt(IWETH.abi, weth);
-        const IERC20 = await hre.artifacts.readArtifact("IERC20");
-        USDT = await ethers.getContractAt(IERC20.abi, usdt);
-        USDC = await ethers.getContractAt(IERC20.abi, usdc);
-        const IUniswapV2Router = await hre.artifacts.readArtifact("IUniswapV2Router");
-        UniswapRouter = await ethers.getContractAt(IUniswapV2Router.abi, uniswapV2Router);
+
+        const ISynthAddressProxy = await hre.artifacts.readArtifact("ISynthAddressProxy");
+        susdProxy = await ethers.getContractAt(ISynthAddressProxy.abi, susd);
+        sethProxy = await ethers.getContractAt(ISynthAddressProxy.abi, seth);
+        slinkProxy = await ethers.getContractAt(ISynthAddressProxy.abi, slink);
+
         // deposit ETH -> WETH
         await WETH.deposit({ value: 5e18.toString() });
-        // WETH -> USDT
-        await WETH.approve(uniswapV2Router, 5e18.toString());
-        await UniswapRouter.swapExactTokensForTokens(5e18.toString(), 0, [weth, usdc], logicOwner.address, Math.floor(Date.now() / 1000 + 10000));
+        // WETH -> susd
+        await WETH.approve(uniswapV2Router.address, 5e18.toString());
+        await uniswapV2Router.swapExactTokensForTokens(5e18.toString(), 0, [weth, susd], logicOwner.address, Math.floor(Date.now() / 1000 + 100000000));
     });
 
     it("Should be able to createFund", async function() {
@@ -121,12 +142,12 @@ describe("PoolFactory", function() {
         });
 
         await expect(poolFactory.createFund(
-            false, manager.address, 'Barren Wuffet', 'Test Fund', "DHTF", new ethers.BigNumber.from('6000'), [[usdc, true], [weth, true]]
+            false, manager.address, 'Barren Wuffet', 'Test Fund', "DHTF", new ethers.BigNumber.from('6000'), [[susd, true], [seth, true]]
         ))
             .to.be.revertedWith('invalid fraction');
 
         let tx = await poolFactory.createFund(
-            false, manager.address, 'Barren Wuffet', 'Test Fund', "DHTF", new ethers.BigNumber.from('5000'), [[usdc, true], [weth, true]]
+            false, manager.address, 'Barren Wuffet', 'Test Fund', "DHTF", new ethers.BigNumber.from('5000'), [[susd, true], [seth, true]]
         );
 
         let event = await fundCreatedEvent;
@@ -158,11 +179,11 @@ describe("PoolFactory", function() {
 
         //default assets are supported
         expect(await poolManagerLogicProxy.numberOfSupportedAssets()).to.equal("2");
-        expect(await poolManagerLogicProxy.isSupportedAsset(usdc)).to.be.true
-        expect(await poolManagerLogicProxy.isSupportedAsset(weth)).to.be.true
+        expect(await poolManagerLogicProxy.isSupportedAsset(susd)).to.be.true
+        expect(await poolManagerLogicProxy.isSupportedAsset(seth)).to.be.true
 
         //Other assets are not supported
-        expect(await poolManagerLogicProxy.isSupportedAsset(usdt)).to.be.false
+        expect(await poolManagerLogicProxy.isSupportedAsset(slink)).to.be.false
 
     });
 
@@ -198,10 +219,10 @@ describe("PoolFactory", function() {
         let totalFundValue = await poolLogicProxy.totalFundValue();
         expect(totalFundValue.toString()).to.equal('0');
 
-        await expect(poolLogicProxy.deposit(usdt, 100e6.toString())).to.be.revertedWith("invalid deposit asset");
+        await expect(poolLogicProxy.deposit(slink, 100e18.toString())).to.be.revertedWith("invalid deposit asset");
 
-        await USDC.approve(poolLogicProxy.address, 100e6.toString());
-        await poolLogicProxy.deposit(usdc, 100e6.toString());
+        await susdProxy.approve(poolLogicProxy.address, 100e18.toString());
+        await poolLogicProxy.deposit(susd, 100e18.toString());
         let event = await depositEvent;
 
         expect(event.fundAddress).to.equal(poolLogicProxy.address);
@@ -216,19 +237,18 @@ describe("PoolFactory", function() {
     it('Should be able to approve', async () => {
         const IERC20 = await hre.artifacts.readArtifact("IERC20");
         const iERC20 = new ethers.utils.Interface(IERC20.abi);
-        let approveABI = iERC20.encodeFunctionData("approve", [usdc, 100e6.toString()]);
-        await expect(poolLogicProxy.connect(manager).execTransaction(usdt, approveABI)).to.be.revertedWith("invalid destination or asset not supported");
+        let approveABI = iERC20.encodeFunctionData("approve", [susd, 100e18.toString()]);
+        await expect(poolLogicProxy.connect(manager).execTransaction(slink, approveABI)).to.be.revertedWith("invalid destination or asset not supported");
 
-        await expect(poolLogicProxy.connect(manager).execTransaction(usdc, approveABI)).to.be.revertedWith("unsupported spender approval");
+        await expect(poolLogicProxy.connect(manager).execTransaction(susd, approveABI)).to.be.revertedWith("unsupported spender approval");
 
-        approveABI = iERC20.encodeFunctionData("approve", [uniswapV2Router, 100e6.toString()]);
-        await poolLogicProxy.connect(manager).execTransaction(usdc, approveABI);
+        approveABI = iERC20.encodeFunctionData("approve", [synthetix.address, 100e18.toString()]);
+        await poolLogicProxy.connect(manager).execTransaction(susd, approveABI);
     });
 
-
-    it("should be able to swap tokens on uniswap.", async () => {
+    it("should be able to swap tokens on synthetix.", async () => {
         let exchangeEvent = new Promise((resolve, reject) => {
-            uniswapV2Guard.on('Exchange', (
+            synthetixGuard.on('Exchange', (
                 managerLogicAddress,
                 sourceAsset,
                 sourceAmount,
@@ -247,43 +267,64 @@ describe("PoolFactory", function() {
 
             setTimeout(() => {
                 reject(new Error('timeout'));
-            }, 60000)
+            }, 600000)
         });
 
-        const sourceAmount = 100e6.toString();
-        const IUniswapV2Router = await hre.artifacts.readArtifact("IUniswapV2Router");
-        const iUniswapV2Router = new ethers.utils.Interface(IUniswapV2Router.abi);
-        let swapABI = iUniswapV2Router.encodeFunctionData("swapExactTokensForTokens", [sourceAmount, 0, [usdc, weth], poolManagerLogicProxy.address, 0]);
-
+        const sourceKey = susdKey;
+        const sourceAmount = (100e18).toString();
+        const destinationKey = sethKey;
+        const daoAddress = await poolFactory.getDaoAddress();
+        const trackingCode = await poolFactory.getTrackingCode();
+    
+        const ISynthetix = await hre.artifacts.readArtifact('ISynthetix');
+        const iSynthetix = new ethers.utils.Interface(ISynthetix.abi);
+        let swapABI = iSynthetix.encodeFunctionData('exchangeWithTracking', [
+            sourceKey,
+            sourceAmount,
+            destinationKey,
+            daoAddress,
+            trackingCode,
+        ]);
+    
         await expect(poolLogicProxy.connect(manager).execTransaction("0x0000000000000000000000000000000000000000", swapABI)).to.be.revertedWith("non-zero address is required");
 
-        swapABI = iUniswapV2Router.encodeFunctionData("swapExactTokensForTokens", [sourceAmount, 0, [usdt, weth], poolLogicProxy.address, 0]);
-        await expect(poolLogicProxy.connect(manager).execTransaction(usdc, swapABI)).to.be.revertedWith("invalid transaction");
+        await expect(poolLogicProxy.connect(manager).execTransaction(susd_price_feed, swapABI)).to.be.revertedWith("invalid destination");
 
-        swapABI = iUniswapV2Router.encodeFunctionData("swapExactTokensForTokens", [sourceAmount, 0, [usdt, weth], poolLogicProxy.address, 0]);
-        await expect(poolLogicProxy.connect(manager).execTransaction(uniswapV2Router, swapABI)).to.be.revertedWith("unsupported source asset");
+        await expect(poolLogicProxy.connect(manager).execTransaction(synthetix.address, "0xaaaaaaaa")).to.be.revertedWith("invalid transaction");
 
-        swapABI = iUniswapV2Router.encodeFunctionData("swapExactTokensForTokens", [sourceAmount, 0, [usdc, user.address, weth], poolLogicProxy.address, 0]);
-        await expect(poolLogicProxy.connect(manager).execTransaction(uniswapV2Router, swapABI)).to.be.revertedWith("invalid routing asset");
+        swapABI = iSynthetix.encodeFunctionData('exchangeWithTracking', [
+            slinkKey,
+            sourceAmount,
+            destinationKey,
+            daoAddress,
+            trackingCode,
+        ]);
+        await expect(poolLogicProxy.connect(manager).execTransaction(synthetix.address, swapABI)).to.be.revertedWith("unsupported source asset");
 
-        swapABI = iUniswapV2Router.encodeFunctionData("swapExactTokensForTokens", [sourceAmount, 0, [usdc, weth, usdt], poolLogicProxy.address, 0]);
-        await expect(poolLogicProxy.connect(manager).execTransaction(uniswapV2Router, swapABI)).to.be.revertedWith("unsupported destination asset");
+        swapABI = iSynthetix.encodeFunctionData('exchangeWithTracking', [
+            sourceKey,
+            sourceAmount,
+            slinkKey,
+            daoAddress,
+            trackingCode,
+        ]);
+        await expect(poolLogicProxy.connect(manager).execTransaction(synthetix.address, swapABI)).to.be.revertedWith("unsupported destination asset");
 
-        swapABI = iUniswapV2Router.encodeFunctionData("swapExactTokensForTokens", [sourceAmount, 0, [usdc, weth], user.address, 0]);
-        await expect(poolLogicProxy.connect(manager).execTransaction(uniswapV2Router, swapABI)).to.be.revertedWith("recipient is not pool");
-
-        swapABI = iUniswapV2Router.encodeFunctionData("swapExactTokensForTokens", [sourceAmount, 0, [usdc, weth], poolLogicProxy.address, 0]);
-        await expect(poolLogicProxy.connect(manager).execTransaction(uniswapV2Router, swapABI)).to.be.revertedWith("failed to execute the call");
-
-        swapABI = iUniswapV2Router.encodeFunctionData("swapExactTokensForTokens", [sourceAmount, 0, [usdc, weth], poolLogicProxy.address, Math.floor(Date.now() / 1000 + 10000)]);
-        await poolLogicProxy.connect(manager).execTransaction(uniswapV2Router, swapABI);
-
-        expect(await USDC.balanceOf(poolLogicProxy.address)).to.be.equal(0);
+        swapABI = iSynthetix.encodeFunctionData('exchangeWithTracking', [
+            sourceKey,
+            sourceAmount,
+            destinationKey,
+            daoAddress,
+            trackingCode,
+        ]);
+        await poolLogicProxy.connect(manager).execTransaction(synthetix.address, swapABI);
+        
+        expect(await sethProxy.balanceOf(poolLogicProxy.address)).to.be.gt(0);
 
         let event = await exchangeEvent;
-        expect(event.sourceAsset).to.equal(usdc);
-        expect(event.sourceAmount).to.equal(100e6.toString());
-        expect(event.destinationAsset).to.equal(weth);
+        expect(event.sourceAsset).to.equal(susd);
+        expect(event.sourceAmount).to.equal(100e18.toString());
+        expect(event.destinationAsset).to.equal(seth);
     });
 
     it('should be able to withdraw', async function() {
@@ -318,8 +359,6 @@ describe("PoolFactory", function() {
 
         // Withdraw 50%
         let withdrawAmount = 50e18
-        let totalSupply = await poolLogicProxy.totalSupply()
-        let totalFundValue = await poolLogicProxy.totalFundValue()
 
         await expect(poolLogicProxy.withdraw(withdrawAmount.toString()))
             .to.be.revertedWith('cooldown active');
@@ -329,13 +368,12 @@ describe("PoolFactory", function() {
         await poolLogicProxy.withdraw(withdrawAmount.toString())
 
         let event = await withdrawalEvent;
-        let valueWithdrawn = withdrawAmount / totalSupply * totalFundValue
         expect(event.fundAddress).to.equal(poolLogicProxy.address);
         expect(event.investor).to.equal(logicOwner.address);
-        checkAlmostSame(event.valueWithdrawn, valueWithdrawn.toString());
-        checkAlmostSame(event.fundTokensWithdrawn, valueWithdrawn.toString());
-        checkAlmostSame(event.totalInvestorFundTokens, valueWithdrawn.toString());
-        checkAlmostSame(event.fundValue, valueWithdrawn.toString());
-        checkAlmostSame(event.totalSupply, (100e18 - withdrawAmount).toString());
+        checkAlmostSame(event.valueWithdrawn, 50e18.toString());
+        checkAlmostSame(event.fundTokensWithdrawn, 50e18.toString());
+        checkAlmostSame(event.totalInvestorFundTokens, 50e18.toString());
+        checkAlmostSame(event.fundValue, 50e18.toString());
+        checkAlmostSame(event.totalSupply, 50e18.toString());
     });
 });
