b055ccb1728a40da766647a7675cc4b07c6421b9
Ermin <ermin@dhedge.org>
2021-06-08 21:44:59 +0800

    test(Sushi LP Asset Guard): Add withdrawal tests

---
+++ test/unit/TestHelpers.js
@@ -0,0 +1,31 @@
+// import hre from "hardhat";
+// import { ethers } from "hardhat";
+// import { Contract } from "@ethersproject/contracts/lib";
+
+const timestamp = async () => {
+  const currentBlockNumber = await ethers.provider.getBlockNumber();
+  return (await ethers.provider.getBlock(currentBlockNumber)).timestamp;
+};
+
+const updateChainlinkAggregators = async (usd_price_feed, eth_price_feed, link_price_feed) => {
+  const MockContract = await ethers.getContractFactory("MockContract");
+
+  const AggregatorV3 = await hre.artifacts.readArtifact("AggregatorV3Interface");
+  const iAggregatorV3 = new ethers.utils.Interface(AggregatorV3.abi);
+  const latestRoundDataABI = iAggregatorV3.encodeFunctionData("latestRoundData", []);
+  const current = await timestamp();
+  await usd_price_feed.givenCalldataReturn(
+    latestRoundDataABI,
+    ethers.utils.solidityPack(["uint256", "int256", "uint256", "uint256", "uint256"], [0, 100000000, 0, current, 0]),
+  ); // $1
+  await eth_price_feed.givenCalldataReturn(
+    latestRoundDataABI,
+    ethers.utils.solidityPack(["uint256", "int256", "uint256", "uint256", "uint256"], [0, 200000000000, 0, current, 0]),
+  ); // $2000
+  await link_price_feed.givenCalldataReturn(
+    latestRoundDataABI,
+    ethers.utils.solidityPack(["uint256", "int256", "uint256", "uint256", "uint256"], [0, 3500000000, 0, current, 0]),
+  ); // $35
+};
+
+module.exports = { updateChainlinkAggregators };
