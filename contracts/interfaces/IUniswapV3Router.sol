14eb76cb31e7e67fceac357af3aae5a02ae5f789
0xCooler <0xcooler@gmail.com>
2021-06-11 22:45:09 +1000

    add(SPDX): add SPDX licenses for all sol files

--- contracts/interfaces/IUniswapV3Router.sol
+++ contracts/interfaces/IUniswapV3Router.sol
@@ -1,12 +1,10 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
+
 pragma solidity 0.6.12;
 pragma experimental ABIEncoderV2;
 
-// import '@uniswap/v3-core/contracts/interfaces/callback/IUniswapV3SwapCallback.sol';
-
 /// @title Router token swapping functionality
 /// @notice Functions for swapping tokens via Uniswap V3
-/*is IUniswapV3SwapCallback*/
 interface IUniswapV3Router {
   struct ExactInputSingleParams {
     address tokenIn;
--- contracts/test/Imports.sol
+++ contracts/test/Imports.sol
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: MIT
+
 pragma solidity 0.6.12;
 
 // We import the contract so truffle compiles it, and we have the ABI
