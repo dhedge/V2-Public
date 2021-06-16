0e61eb39cb79cffc21714a52ce58a29539ce3560
0xCooler <0xcooler@gmail.com>
2021-06-17 01:28:48 +1000

    deploy(mumbai): prepare mumbai deployment

--- scripts/Helpers.js
+++ scripts/Helpers.js
@@ -3,9 +3,11 @@ const { exec } = require("child_process");
 const execProm = util.promisify(exec);
 
 const getTag = async () => {
-  await execProm("git pull --tags");
+  try {
+    await execProm("git pull --tags");
+  } catch {}
   let result = await execProm("git tag | sort -V | tail -1");
-  return result.stdout.trim()
-}
+  return result.stdout.trim();
+};
 
 module.exports = { getTag };
