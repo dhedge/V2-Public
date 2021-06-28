483471af0cffd0efa2a4412194980646d11764e2
0xCooler <0xcooler@gmail.com>
2021-06-29 01:53:34 +1000

    feat(AaveLendingPoolAssetGuard): add asset guard for aave lending pool

---
+++ contracts/guards/assetGuards/AaveLendingPoolAssetGuard.sol
@@ -0,0 +1,55 @@
+//        __  __    __  ________  _______    ______   ________
+//       /  |/  |  /  |/        |/       \  /      \ /        |
+//   ____$$ |$$ |  $$ |$$$$$$$$/ $$$$$$$  |/$$$$$$  |$$$$$$$$/
+//  /    $$ |$$ |__$$ |$$ |__    $$ |  $$ |$$ | _$$/ $$ |__
+// /$$$$$$$ |$$    $$ |$$    |   $$ |  $$ |$$ |/    |$$    |
+// $$ |  $$ |$$$$$$$$ |$$$$$/    $$ |  $$ |$$ |$$$$ |$$$$$/
+// $$ \__$$ |$$ |  $$ |$$ |_____ $$ |__$$ |$$ \__$$ |$$ |_____
+// $$    $$ |$$ |  $$ |$$       |$$    $$/ $$    $$/ $$       |
+//  $$$$$$$/ $$/   $$/ $$$$$$$$/ $$$$$$$/   $$$$$$/  $$$$$$$$/
+//
+// dHEDGE DAO - https://dhedge.org
+//
+// Copyright (c) 2021 dHEDGE DAO
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//
+// SPDX-License-Identifier: BUSL-1.1
+
+pragma solidity 0.7.6;
+
+import "./ERC20Guard.sol";
+import "../../interfaces/aave/ILendingPool.sol";
+import "@openzeppelin/contracts-upgradeable/math/SafeMathUpgradeable.sol";
+
+/// @title Aave lending pool asset guard
+/// @dev Asset type = 3
+contract AaveLendingPoolAssetGuard is TxDataUtils, ERC20Guard {
+  using SafeMathUpgradeable for uint256;
+
+  constructor() {}
+
+  /// @notice Returns the pool position of Aave lending pool
+  /// @dev Returns the balance priced in ETH
+  /// @param pool The pool logic address
+  /// @param lendingPool The Aave lending pool
+  function getBalance(address pool, address lendingPool) external view override returns (uint256 balance) {
+    (uint256 totalCollateralETH, uint256 totalDebtETH, , , , ) = ILendingPool(lendingPool).getUserAccountData(pool);
+    balance = totalCollateralETH.sub(totalDebtETH);
+  }
+}
--- contracts/interfaces/aave/ILendingPool.sol
+++ contracts/interfaces/aave/ILendingPool.sol
@@ -42,4 +42,16 @@ interface ILendingPool {
   function swapBorrowRateMode(address asset, uint256 rateMode) external;
 
   function getUserConfiguration(address user) external view returns (UserConfigurationMap memory);
+
+  function getUserAccountData(address user)
+    external
+    view
+    returns (
+      uint256 totalCollateralETH,
+      uint256 totalDebtETH,
+      uint256 availableBorrowsETH,
+      uint256 currentLiquidationThreshold,
+      uint256 ltv,
+      uint256 healthFactor
+    );
 }
