318278172ca4dc9bcf1e9bc50ba26137f026755d
Ermin <ermin@dhedge.org>
2021-08-03 12:24:59 +0800

    test(Unit Test): Update PoolFactoryV24 to deployed version

--- contracts/v2.4.0/PoolFactoryV24.sol
+++ contracts/v2.4.0/PoolFactoryV24.sol
@@ -111,7 +111,6 @@ contract PoolFactoryV24 is
   uint256 internal _daoFeeDenominator;
 
   mapping(address => bool) public isPool;
-  mapping(address => bool) public isPoolManager;
 
   uint256 private _MAXIMUM_MANAGER_FEE_NUMERATOR;
   uint256 private _MANAGER_FEE_DENOMINATOR;
@@ -126,6 +125,12 @@ contract PoolFactoryV24 is
   uint256 public override maximumManagerFeeNumeratorChange;
   uint256 public override managerFeeNumeratorChangeDelay;
 
+  /// @notice Initialize the factory
+  /// @param _poolLogic The pool logic address
+  /// @param _managerLogic The manager logic address
+  /// @param assetHandler The address of the asset handler
+  /// @param _daoAddress The address of the DAO
+  /// @param _governanceAddress The address of the governance contract
   function initialize(
     address _poolLogic,
     address _managerLogic,
@@ -154,6 +159,15 @@ contract PoolFactoryV24 is
     _setPoolStorageVersion(230); // V2.3.0;
   }
 
+  /// @notice Function to create a new fund
+  /// @param _privatePool A boolean indicating whether the fund is private or not
+  /// @param _manager A manager address
+  /// @param _managerName The name of the manager
+  /// @param _fundName The name of the fund
+  /// @param _fundSymbol The symbol of the fund
+  /// @param _managerFeeNumerator The numerator of the manager fee
+  /// @param _supportedAssets An array of supported assets
+  /// @return fund Address of the fund
   function createFund(
     bool _privatePool,
     address _manager,
@@ -162,7 +176,7 @@ contract PoolFactoryV24 is
     string memory _fundSymbol,
     uint256 _managerFeeNumerator,
     IHasSupportedAssetV24.Asset[] memory _supportedAssets
-  ) external returns (address) {
+  ) external returns (address fund) {
     require(!paused(), "contracts paused");
     require(_supportedAssets.length <= _maximumSupportedAssetCount, "maximum assets reached");
     require(_managerFeeNumerator <= _MAXIMUM_MANAGER_FEE_NUMERATOR, "invalid manager fee");
@@ -175,7 +189,7 @@ contract PoolFactoryV24 is
       _fundSymbol
     );
 
-    address fund = deploy(poolLogicData, 2);
+    fund = deploy(poolLogicData, 2);
 
     bytes memory managerLogicData = abi.encodeWithSignature(
       "initialize(address,address,string,address,uint256,(address,bool)[])",
@@ -193,7 +207,6 @@ contract PoolFactoryV24 is
 
     deployedFunds.push(fund);
     isPool[fund] = true;
-    isPoolManager[managerLogic] = true;
 
     poolVersion[fund] = poolStorageVersion;
 
@@ -207,16 +220,18 @@ contract PoolFactoryV24 is
       _managerFeeNumerator,
       _MANAGER_FEE_DENOMINATOR
     );
-
-    return fund;
   }
 
   // DAO info (Uber Pool)
 
+  /// @notice Set the DAO address
+  /// @param _daoAddress The address of the DAO
   function setDAOAddress(address _daoAddress) external onlyOwner {
     _setDAOAddress(_daoAddress);
   }
 
+  /// @notice Set the DAO address internal call
+  /// @param _daoAddress The address of the DAO
   function _setDAOAddress(address _daoAddress) internal {
     require(_daoAddress != address(0), "Invalid daoAddress");
 
@@ -227,10 +242,14 @@ contract PoolFactoryV24 is
 
   // Governance info
 
+  /// @notice Set the governance address
+  /// @param _governanceAddress The address of the governance contract
   function setGovernanceAddress(address _governanceAddress) external onlyOwner {
     _setGovernanceAddress(_governanceAddress);
   }
 
+  /// @notice Set the governance address internal call
+  /// @param _governanceAddress The address of the governance contract
   function _setGovernanceAddress(address _governanceAddress) internal {
     require(_governanceAddress != address(0), "Invalid governanceAddress");
 
@@ -239,10 +258,16 @@ contract PoolFactoryV24 is
     emit GovernanceAddressSet(_governanceAddress);
   }
 
+  /// @notice Set the DAO fee
+  /// @param numerator The numerator of the DAO fee
+  /// @param denominator The denominator of the DAO fee
   function setDaoFee(uint256 numerator, uint256 denominator) external onlyOwner {
     _setDaoFee(numerator, denominator);
   }
 
+  /// @notice Set the DAO fee internal call
+  /// @param numerator The numerator of the DAO fee
+  /// @param denominator The denominator of the DAO fee
   function _setDaoFee(uint256 numerator, uint256 denominator) internal {
     require(numerator <= denominator, "invalid fraction");
 
@@ -252,25 +277,31 @@ contract PoolFactoryV24 is
     emit DaoFeeSet(numerator, denominator);
   }
 
+  /// @notice Get the DAO fee
+  /// @return The numerator of the DAO fee
+  /// @return The denominator of the DAO fee
   function getDaoFee() external view override returns (uint256, uint256) {
     return (_daoFeeNumerator, _daoFeeDenominator);
   }
 
-  modifier onlyPoolManager() {
-    require(isPoolManager[msg.sender], "Not a pool manager");
-    _;
-  }
-
   // Manager fees
 
+  /// @notice Get the maximum manager fee
+  /// @return The maximum manager fee numerator
+  /// @return The maximum manager fee denominator
   function getMaximumManagerFee() external view override returns (uint256, uint256) {
     return (_MAXIMUM_MANAGER_FEE_NUMERATOR, _MANAGER_FEE_DENOMINATOR);
   }
 
+  /// @notice Set the maximum manager fee
+  /// @param numerator The numerator of the maximum manager fee
   function setMaximumManagerFee(uint256 numerator) external onlyOwner {
     _setMaximumManagerFee(numerator, _MANAGER_FEE_DENOMINATOR);
   }
 
+  /// @notice Set the maximum manager fee internal call
+  /// @param numerator The numerator of the maximum manager fee
+  /// @param denominator The denominator of the maximum manager fee
   function _setMaximumManagerFee(uint256 numerator, uint256 denominator) internal {
     require(numerator <= denominator, "invalid fraction");
 
@@ -280,71 +311,98 @@ contract PoolFactoryV24 is
     emit SetMaximumManagerFee(numerator, denominator);
   }
 
+  /// @notice Set maximum manager fee numberator change
+  /// @param amount The amount for the maximum manager fee numerator change
   function setMaximumManagerFeeNumeratorChange(uint256 amount) public onlyOwner {
     maximumManagerFeeNumeratorChange = amount;
 
     emit SetMaximumManagerFeeNumeratorChange(amount);
   }
 
+  /// @notice Set manager fee numberator change delay
+  /// @param delay The delay in seconds for the manager fee numerator change
   function setManagerFeeNumeratorChangeDelay(uint256 delay) public onlyOwner {
     managerFeeNumeratorChangeDelay = delay;
 
     emit SetManagerFeeNumeratorChangeDelay(delay);
   }
 
+  /// @notice Set exit cool down time (in seconds)
+  /// @param cooldown The cool down time in seconds
   function setExitCooldown(uint256 cooldown) external onlyOwner {
     _setExitCooldown(cooldown);
   }
 
+  /// @notice Set exit cool down time (in seconds) internal call
+  /// @param cooldown The cool down time in seconds
   function _setExitCooldown(uint256 cooldown) internal {
     _exitCooldown = cooldown;
 
     emit ExitCooldownSet(cooldown);
   }
 
+  /// @notice Get the exit cool down time (in seconds)
+  /// @return The exit cool down time in seconds
   function getExitCooldown() external view override returns (uint256) {
     return _exitCooldown;
   }
 
   // Asset Info
 
+  /// @notice Set maximum supported asset count
+  /// @param count The maximum supported asset count
   function setMaximumSupportedAssetCount(uint256 count) external onlyOwner {
     _setMaximumSupportedAssetCount(count);
   }
 
+  /// @notice Set maximum supported asset count internal call
+  /// @param count The maximum supported asset count
   function _setMaximumSupportedAssetCount(uint256 count) internal {
     _maximumSupportedAssetCount = count;
 
     emit MaximumSupportedAssetCountSet(count);
   }
 
+  /// @notice Get maximum supported asset count
+  /// @return The maximum supported asset count
   function getMaximumSupportedAssetCount() external view virtual override returns (uint256) {
     return _maximumSupportedAssetCount;
   }
 
+  /// @notice Return boolean if the asset is supported
+  /// @return True if it's valid asset, false otherwise
   function isValidAsset(address asset) public view override returns (bool) {
     return IAssetHandler(_assetHandler).priceAggregators(asset) != address(0);
   }
 
-  /**
-   * Returns the latest price of a given asset
-   */
-  function getAssetPrice(address asset) external view override returns (uint256) {
-    return IAssetHandler(_assetHandler).getUSDPrice(asset);
+  /// @notice Return the latest price of a given asset
+  /// @param asset The address of the asset
+  /// @return price The latest price of a given asset
+  function getAssetPrice(address asset) external view override returns (uint256 price) {
+    price = IAssetHandler(_assetHandler).getUSDPrice(asset);
   }
 
-  function getAssetType(address asset) external view override returns (uint16) {
-    return IAssetHandler(_assetHandler).assetTypes(asset);
+  /// @notice Return type of the asset
+  /// @param asset The address of the asset
+  /// @return assetType The type of the asset
+  function getAssetType(address asset) external view override returns (uint16 assetType) {
+    assetType = IAssetHandler(_assetHandler).assetTypes(asset);
   }
 
+  /// @notice Return the address of the asset handler
+  /// @return Address of the asset handler
   function getAssetHandler() external view returns (address) {
     return _assetHandler;
   }
 
+  /// @notice Set the asset handler address
+  /// @param assetHandler The address of the asset handler
   function setAssetHandler(address assetHandler) external onlyOwner {
     _setAssetHandler(assetHandler);
   }
 
+  /// @notice Set the asset handler address internal call
+  /// @param assetHandler The address of the asset handler
   function _setAssetHandler(address assetHandler) internal {
     require(assetHandler != address(0), "Invalid assetHandler");
 
@@ -355,10 +413,14 @@ contract PoolFactoryV24 is
 
   // Upgrade
 
+  /// @notice Set the pool storage version
+  /// @param _poolStorageVersion The pool storage version
   function setPoolStorageVersion(uint256 _poolStorageVersion) external onlyOwner {
     _setPoolStorageVersion(_poolStorageVersion);
   }
 
+  /// @notice Set the pool storage version internal call
+  /// @param _poolStorageVersion The pool storage version
   function _setPoolStorageVersion(uint256 _poolStorageVersion) internal {
     require(_poolStorageVersion > poolStorageVersion, "version needs to be higher");
 
@@ -368,7 +430,7 @@ contract PoolFactoryV24 is
   }
 
   /**
-   * @dev Backdoor function
+   * @notice Backdoor function
    * @param pool Address of the target.
    * @param data Calldata for the target address.
    * @param targetVersion set target version after call
@@ -396,6 +458,12 @@ contract PoolFactoryV24 is
     poolVersion[pool] = targetVersion;
   }
 
+  /// @notice Upgrade pools in batch
+  /// @param startIndex The start index of the pool upgrade
+  /// @param endIndex The end index of the pool upgrade
+  /// @param sourceVersion The source version of the pool upgrade
+  /// @param targetVersion The target version of the pool upgrade
+  /// @param data The calldata for the target address
   function upgradePoolBatch(
     uint256 startIndex,
     uint256 endIndex,
@@ -414,20 +482,27 @@ contract PoolFactoryV24 is
     }
   }
 
+  /// @notice call the pause the contract
   function pause() external onlyOwner {
     _pause();
   }
 
+  /// @notice call the unpause the contract
   function unpause() external onlyOwner {
     _unpause();
   }
 
+  /// @notice Return the pause status
+  /// @return The pause status
   function isPaused() external view override returns (bool) {
     return paused();
   }
 
   // Transaction Guards
 
+  /// @notice Get address of the transaction guard
+  /// @param extContract The address of the external contract
+  /// @return guard Return the address of the transaction guard
   function getGuard(address extContract) external view override returns (address guard) {
     guard = IGovernanceV24(governanceAddress).contractGuards(extContract);
     if (guard == address(0)) {
@@ -435,20 +510,26 @@ contract PoolFactoryV24 is
     }
   }
 
-  function getAssetGuard(address extContract) public view override returns (address guard) {
-    if (isValidAsset(extContract)) {
-      uint16 assetType = IAssetHandler(_assetHandler).assetTypes(extContract);
+  /// @notice Get address of the asset guard
+  /// @param extAsset The address of the external asset
+  /// @return guard Address of the asset guard
+  function getAssetGuard(address extAsset) public view override returns (address guard) {
+    if (isValidAsset(extAsset)) {
+      uint16 assetType = IAssetHandler(_assetHandler).assetTypes(extAsset);
       guard = IGovernanceV24(governanceAddress).assetGuards(assetType);
     }
   }
 
+  /// @notice Get address from the Governance contract
+  /// @param name The name of the address
+  /// @return destination The destination address
   function getAddress(bytes32 name) external view override returns (address destination) {
     destination = IGovernanceV24(governanceAddress).nameToDestination(name);
     require(destination != address(0), "governance: invalid name");
   }
 
   /// @notice Return full array of deployed funds
-  /// @return full array of deployed funds
+  /// @return Full array of deployed funds
   function getDeployedFunds() external view returns (address[] memory) {
     return deployedFunds;
   }
@@ -456,10 +537,11 @@ contract PoolFactoryV24 is
   /**
    * @notice Returns all invested pools by a given user
    * @param user the user address
+   * @return investedPools All invested pools by a given user
    */
-  function getInvestedPools(address user) external view returns (address[] memory) {
+  function getInvestedPools(address user) external view returns (address[] memory investedPools) {
     uint256 length = deployedFunds.length;
-    address[] memory investedPools = new address[](length);
+    investedPools = new address[](length);
     uint256 index = 0;
     for (uint256 i = 0; i < length; i++) {
       if (IERC20Upgradeable(deployedFunds[i]).balanceOf(user) > 0) {
@@ -472,16 +554,16 @@ contract PoolFactoryV24 is
     assembly {
       mstore(investedPools, sub(mload(investedPools), reduceLength))
     }
-    return investedPools;
   }
 
   /**
    * @notice Returns all managed pools by a given manager
-   * @param manager the manager address
+   * @param manager The manager address
+   * @return managedPools All managed pools by a given manager
    */
-  function getManagedPools(address manager) external view returns (address[] memory) {
+  function getManagedPools(address manager) external view returns (address[] memory managedPools) {
     uint256 length = deployedFunds.length;
-    address[] memory managedPools = new address[](length);
+    managedPools = new address[](length);
     uint256 index = 0;
     for (uint256 i = 0; i < length; i++) {
       address poolManagerLogic = IPoolLogicV24(deployedFunds[i]).poolManagerLogic();
@@ -495,7 +577,6 @@ contract PoolFactoryV24 is
     assembly {
       mstore(managedPools, sub(mload(managedPools), reduceLength))
     }
-    return managedPools;
   }
 
   uint256[50] private __gap;
