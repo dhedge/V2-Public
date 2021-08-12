d72ca8fb27678550a968be984982cd8e6aa54b2e
Lecky <leckylao@gmail.com>
2021-08-12 15:12:03 +1000

    looks like missing await, revert to use function

--- scripts/upgrade.js
+++ scripts/upgrade.js
@@ -4,9 +4,34 @@ const Safe = require("@gnosis.pm/safe-core-sdk");
 const { EthersAdapter } = require('@gnosis.pm/safe-core-sdk');
 const { SafeService } = require("@gnosis.pm/safe-ethers-adapters");
 const proxyAdminAddress = "0x0C0a10C9785a73018077dBC74B2A006695849252";
+const safeAddress = "0xc715Aa67866A2FEF297B12Cb26E953481AeD2df4";
 // https://github.com/gnosis/safe-deployments/blob/main/src/assets/v1.3.0/multi_send.json#L13
 const multiSendAddress = "0xA238CBeb142c10Ef7Ad8442C6D1f9E89e07e7761";
 const service = new SafeService("https://safe-transaction.polygon.gnosis.io");
+
+const proposeTx = async(hre, oldAddress, implementation) => {
+  const ProxyAdmin = await hre.artifacts.readArtifact("ProxyAdmin");
+  const proxyAdmin = new ethers.utils.Interface(ProxyAdmin.abi);
+  const upgradeABI = proxyAdmin.encodeFunctionData("upgrade", [oldAddress, implementation]);
+
+  const transaction = {
+    to: proxyAdminAddress,
+    value: "0",
+    data: upgradeABI,
+  };
+
+  const safeTransaction = await safeSdk.createTransaction(...[transaction])
+  // off-chain sign
+  const txHash = await safeSdk.getTransactionHash(safeTransaction);
+  const signature = await safeSdk.signTransactionHash(txHash);
+  // on-chain sign
+  // const approveTxResponse = await safeSdk.approveTransactionHash(txHash)
+  // console.log("approveTxResponse", approveTxResponse);
+  console.log("safeTransaction: ", safeTransaction);
+
+  const proposeTx = await service.proposeTx(safeAddress, txHash, safeTransaction, signature)
+  console.log("ProposeTx: ", proposeTx);
+};
 
 task("upgrade", "Upgrade proxy contracts")
   .addOptionalParam("poolFactory", "upgrade poolFactory", false, types.boolean)
@@ -20,7 +45,6 @@ task("upgrade", "Upgrade proxy contracts")
     const ethAdapter = new EthersAdapter({ ethers: ethers, signer: owner1 });
     const chainId = await ethAdapter.getChainId();
     const hre = require("hardhat");
-    const safeAddress = "0xc715Aa67866A2FEF297B12Cb26E953481AeD2df4";
 
     const contractNetworks = {
       [chainId]: {
@@ -56,37 +80,19 @@ task("upgrade", "Upgrade proxy contracts")
     versions[newTag].network = network;
     versions[newTag].date = new Date().toUTCString();
     const PoolFactory = await ethers.getContractFactory("PoolFactory");
-    const ProxyAdmin = await hre.artifacts.readArtifact("ProxyAdmin");
-    const proxyAdmin = new ethers.utils.Interface(ProxyAdmin.abi);
     let setLogic = false;
 
     if(taskArgs.poolFactory){
       let poolFactoryProxy = contracts.PoolFactoryProxy;
       const newPoolFactoryLogic = await upgrades.prepareUpgrade(poolFactoryProxy, PoolFactory);
       console.log("New PoolFactory logic deployed to: ", newPoolFactoryLogic);
-      const upgradeABI = proxyAdmin.encodeFunctionData("upgrade", [poolFactoryProxy, newPoolFactoryLogic]);
 
       await hre.run("verify:verify", {
         address: newPoolFactoryLogic,
         contract: "contracts/PoolFactory.sol:PoolFactory",
       });
 
-      const transaction = {
-        to: proxyAdminAddress,
-        value: "0",
-        data: upgradeABI,
-      };
-
-      const safeTransaction = await safeSdk.createTransaction(...[transaction])
-      // off-chain sign
-      const txHash = await safeSdk.getTransactionHash(safeTransaction);
-      const signature = await safeSdk.signTransactionHash(txHash);
-      // on-chain sign
-      // const approveTxResponse = await safeSdk.approveTransactionHash(txHash)
-      // console.log("approveTxResponse", approveTxResponse);
-      console.log("safeTransaction: ", safeTransaction);
-
-      const proposeTx = await service.proposeTx(safeAddress, txHash, safeTransaction, signature)
+      const proposeTx = await proposeTx(hre, poolFactoryProxy, newPoolFactoryLogic);
       console.log("ProposeTx: ", proposeTx);
     }
     if(taskArgs.assetHandler){
@@ -94,29 +100,13 @@ task("upgrade", "Upgrade proxy contracts")
       const AssetHandler = await ethers.getContractFactory("AssetHandler");
       const assetHandler = await upgrades.prepareUpgrade(oldAssetHandler, AssetHandler);
       console.log("assetHandler logic deployed to: ", assetHandler);
-      const upgradeABI = proxyAdmin.encodeFunctionData("upgrade", [oldAssetHandler, assetHandler]);
 
       await hre.run("verify:verify", {
         address: assetHandler,
         contract: "contracts/assets/AssetHandler.sol:AssetHandler",
       });
 
-      const transaction = {
-        to: proxyAdminAddress,
-        value: "0",
-        data: upgradeABI,
-      };
-
-      const safeTransaction = await safeSdk.createTransaction(...[transaction])
-      // off-chain sign
-      const txHash = await safeSdk.getTransactionHash(safeTransaction);
-      const signature = await safeSdk.signTransactionHash(txHash);
-      // on-chain sign
-      // const approveTxResponse = await safeSdk.approveTransactionHash(txHash)
-      // console.log("approveTxResponse", approveTxResponse);
-      console.log("safeTransaction: ", safeTransaction);
-
-      const proposeTx = await service.proposeTx(safeAddress, txHash, safeTransaction, signature)
+      const proposeTx = await proposeTx(hre, oldAssetHandler, assetHandler);
       console.log("ProposeTx: ", proposeTx);
     }
     if(taskArgs.poolLogic){
