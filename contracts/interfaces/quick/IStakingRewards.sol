19f41c2e7136026db96b91934ee9240c31383048
0xcooler <0xcooler@gmail.com>
2021-08-16 10:22:31 -0400

    prettier

--- contracts/interfaces/quick/IStakingRewards.sol
+++ contracts/interfaces/quick/IStakingRewards.sol
@@ -13,9 +13,9 @@ interface IStakingRewards {
 
   function balanceOf(address account) external view returns (uint256);
 
-  function stakingToken() external view returns(address);
+  function stakingToken() external view returns (address);
 
-  function rewardsToken() external view returns(address);
+  function rewardsToken() external view returns (address);
 
   function stake(uint256 amount) external;
 
--- contracts/interfaces/quick/IStakingRewardsFactory.sol
+++ contracts/interfaces/quick/IStakingRewardsFactory.sol
@@ -3,7 +3,7 @@ pragma solidity 0.7.6;
 pragma experimental ABIEncoderV2;
 
 interface IStakingRewardsFactory {
-  function rewardsToken() external view returns(address);
+  function rewardsToken() external view returns (address);
 
   function stakingRewardsInfoByStakingToken(address stakingToken)
     external
