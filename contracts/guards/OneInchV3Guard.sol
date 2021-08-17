2c21b378475420a4be14039a7725ddf31e6558ae
0xcooler <0xcooler@gmail.com>
2021-08-17 10:49:01 -0400

    feat(OneInchV3Guard): Add 1Inch V3 guard

---
+++ contracts/guards/OneInchV3Guard.sol
@@ -0,0 +1,88 @@
+//        __  __    __  ________  _______    ______   ________
+//       /  |/  |  /  |/        |/       \  /      \ /        |
+//   ____$$ |$$ |  $$ |$$$$$$$$/ $$$$$$$  |/$$$$$$  |$$$$$$$$/
+//  /    $$ |$$ |__$$ |$$ |__    $$ |  $$ |$$ | _$$/ $$ |__
+// /$$$$$$$ |$$    $$ |$$    |   $$ |  $$ |$$ |/    |$$    |
+// $$ |  $$ |$$$$$$$$ |$$$$$/    $$ |  $$ |$$ |$$$$ |$$$$$/
+// $$ \__$$ |$$ |  $$ |$$ |_____ $$ |__$$ |$$ \__$$ |$$ |_____
+// $$    $$ |$$ |  $$ |$$       |$$    $$/ $$    $$/ $$       |
+//  $$$$$$$/ $$/   $$/ $$$$$$$$/ $$$$$$$/   $$$$$$/  $$$$$$$$/
+//
+// dHEDGE DAO - https://dhedge.org
+//
+// Copyright (c) 2021 dHEDGE DAO
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//
+// SPDX-License-Identifier: BUSL-1.1
+
+pragma solidity 0.7.6;
+
+import "@openzeppelin/contracts-upgradeable/math/SafeMathUpgradeable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+import "../utils/TxDataUtils.sol";
+import "../interfaces/guards/IGuard.sol";
+import "../interfaces/IPoolManagerLogic.sol";
+import "../interfaces/IHasSupportedAsset.sol";
+import "../interfaces/IManaged.sol";
+
+/// @notice Transaction guard for UniswapV2Router
+/// @dev This will be used for sushiswap as well since Sushi uses the same interface.
+contract UniswapV2RouterGuard is TxDataUtils, IGuard, Ownable {
+  using SafeMathUpgradeable for uint256;
+
+  /// @notice Transaction guard for Uniswap V2
+  /// @dev It supports swap functionalities
+  /// @param _poolManagerLogic the pool manager logic
+  /// @param data the transaction data
+  /// @return txType the transaction type of a given transaction data. 2 for `Exchange` type
+  /// @return isPublic if the transaction is public or private
+  function txGuard(
+    address _poolManagerLogic,
+    address, // to
+    bytes calldata data
+  )
+    external
+    override
+    returns (
+      uint16 txType, // transaction type
+      bool // isPublic
+    )
+  {
+    IPoolManagerLogic poolManagerLogic = IPoolManagerLogic(_poolManagerLogic);
+    IHasSupportedAsset poolManagerLogicAssets = IHasSupportedAsset(_poolManagerLogic);
+
+    bytes4 method = getMethod(data);
+
+    if (method == 0x7c025200) {
+      // 1inch's `swap` method
+      address srcAsset = convert32toAddress(getInput(data, 3));
+      address dstAsset = convert32toAddress(getInput(data, 4));
+      uint256 srcAmount = uint256(getInput(data, 7));
+
+      require(poolManagerLogicAssets.isSupportedAsset(dstAsset), "unsupported destination asset");
+
+      emit ExchangeFrom(poolManagerLogic.poolLogic(), srcAsset, uint256(srcAmount), dstAsset, block.timestamp);
+
+      txType = 2; // 'Exchange' type
+    }
+
+    return (txType, false);
+  }
+}
