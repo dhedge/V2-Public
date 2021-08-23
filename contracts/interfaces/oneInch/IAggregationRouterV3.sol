a0be3ba83ef580fcb39be5ade0fded6afbf688bd
0xcooler <0xcooler@gmail.com>
2021-08-23 13:35:03 -0400

    prettier

--- contracts/interfaces/oneInch/IAggregationRouterV3.sol
+++ contracts/interfaces/oneInch/IAggregationRouterV3.sol
@@ -6,26 +6,33 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 interface IAggregationRouterV3 {
   struct SwapDescription {
-      IERC20 srcToken;
-      IERC20 dstToken;
-      address srcReceiver;
-      address dstReceiver;
-      uint256 amount;
-      uint256 minReturnAmount;
-      uint256 flags;
-      bytes permit;
+    IERC20 srcToken;
+    IERC20 dstToken;
+    address srcReceiver;
+    address dstReceiver;
+    uint256 amount;
+    uint256 minReturnAmount;
+    uint256 flags;
+    bytes permit;
   }
 
   function swap(
     address caller,
     SwapDescription calldata desc,
     bytes calldata data
-  ) external payable returns (uint256 returnAmount, uint256 gasLeft, uint256 chiSpent);
+  )
+    external
+    payable
+    returns (
+      uint256 returnAmount,
+      uint256 gasLeft,
+      uint256 chiSpent
+    );
 
   function unoswap(
     IERC20 srcToken,
     uint256 amount,
     uint256 minReturn,
     bytes32[] calldata pools
-  ) external payable returns(uint256 returnAmount);
+  ) external payable returns (uint256 returnAmount);
 }
