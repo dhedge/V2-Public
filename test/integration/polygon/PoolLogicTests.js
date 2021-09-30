9b0d1fbf366956874ae0b2d3668e8a2bc2bd0d47
Jake Mc <jake.mc@icloud.com>
2021-09-30 15:18:32 +1000

    (fix) simplify the test bed

--- test/integration/polygon/PoolLogicTests.js
+++ test/integration/polygon/PoolLogicTests.js
@@ -3,17 +3,17 @@ const { BigNumber } = ethers;
 const { expect, use } = require("chai");
 const { solidity } = require("ethereum-waffle");
 const { units } = require("../../TestHelpers");
-const { sushi, aave, assets, price_feeds } = require("../polygon-data");
+const { aave, assets, price_feeds } = require("../polygon-data");
 
 use(solidity);
 
 const oneDollar = units(1);
 
-describe("PoolPerformance", function () {
-  let USDC;
+describe("PoolLogic", function () {
+  let WMatic;
   let logicOwner, manager, dao;
   let PoolLogic;
-  let assetHandler, governance, poolFactory, poolLogicProxy, poolPerformance;
+  let poolFactory, poolLogicProxy;
 
   beforeEach(async function () {
     [logicOwner, manager, dao] = await ethers.getSigners();
@@ -21,10 +21,10 @@ describe("PoolPerformance", function () {
     const AssetHandlerLogic = await ethers.getContractFactory("AssetHandler");
 
     const Governance = await ethers.getContractFactory("Governance");
-    governance = await Governance.deploy();
+    const governance = await Governance.deploy();
 
     const PoolPerformance = await ethers.getContractFactory("PoolPerformance");
-    poolPerformance = await upgrades.deployProxy(PoolPerformance, [aave.protocolDataProvider]);
+    const poolPerformance = await upgrades.deployProxy(PoolPerformance, [aave.protocolDataProvider]);
     await poolPerformance.deployed();
 
     PoolLogic = await ethers.getContractFactory("PoolLogic");
@@ -35,10 +35,9 @@ describe("PoolPerformance", function () {
 
     // Initialize Asset Price Consumer
     const assetWmatic = { asset: assets.wmatic, assetType: 0, aggregator: price_feeds.matic };
-    const assetUsdc = { asset: assets.usdc, assetType: 0, aggregator: price_feeds.usdc };
-    const assetHandlerInitAssets = [assetWmatic, assetUsdc];
+    const assetHandlerInitAssets = [assetWmatic];
 
-    assetHandler = await upgrades.deployProxy(AssetHandlerLogic, [assetHandlerInitAssets]);
+    const assetHandler = await upgrades.deployProxy(AssetHandlerLogic, [assetHandlerInitAssets]);
     await assetHandler.deployed();
     await assetHandler.setChainlinkTimeout((3600 * 24 * 365).toString()); // 1 year expiry
 
@@ -62,23 +61,9 @@ describe("PoolPerformance", function () {
 
     // Setup LogicOwner with some USDC
     const IWETH = await hre.artifacts.readArtifact("IWETH");
-    const WMatic = await ethers.getContractAt(IWETH.abi, assets.wmatic);
-    const IERC20 = await hre.artifacts.readArtifact("IERC20");
-    USDC = await ethers.getContractAt(IERC20.abi, assets.usdc);
+    WMatic = await ethers.getContractAt(IWETH.abi, assets.wmatic);
 
-    const IUniswapV2Router = await hre.artifacts.readArtifact("IUniswapV2Router");
-    const sushiswapRouter = await ethers.getContractAt(IUniswapV2Router.abi, sushi.router);
     await WMatic.deposit({ value: units(1000) });
-
-    // Get USDC
-    await WMatic.approve(sushi.router, units(1000));
-    await sushiswapRouter.swapExactTokensForTokens(
-      units(500),
-      0,
-      [assets.wmatic, assets.usdc],
-      logicOwner.address,
-      Math.floor(Date.now() / 1000 + 100000000),
-    );
   });
 
   // Checks to make sure early exit of 100% drains all assets and does not incur fee
@@ -88,14 +73,14 @@ describe("PoolPerformance", function () {
     const managerFee = BigNumber.from("0"); // 0%;
     // Create the fund we're going to use for testing
     await poolFactory.createFund(false, manager.address, "Barren Wuffet", "Test Fund", "DHTF", managerFee, [
-      [assets.usdc, true],
+      [assets.wmatic, true],
     ]);
 
     const funds = await poolFactory.getDeployedFunds();
     poolLogicProxy = await PoolLogic.attach(funds[0]);
     // Deposit $1 conventional way
-    await USDC.approve(poolLogicProxy.address, (100e6).toString());
-    await poolLogicProxy.deposit(assets.usdc, (100e6).toString());
+    await WMatic.approve(poolLogicProxy.address, units(500));
+    await poolLogicProxy.deposit(assets.wmatic, units(500));
 
     // Check token price is $1
     expect((await poolLogicProxy.tokenPrice()).toString()).to.equal(oneDollar.toString());
@@ -112,14 +97,10 @@ describe("PoolPerformance", function () {
     expect((await poolLogicProxy.tokenPrice()).toString()).to.equal("0");
 
     // We deposit again to make sure everything is reset
-    await USDC.approve(poolLogicProxy.address, (100e6).toString());
-    await poolLogicProxy.deposit(assets.usdc, (100e6).toString());
+    await WMatic.approve(poolLogicProxy.address, units(500));
+    await poolLogicProxy.deposit(assets.wmatic, units(500));
 
     // Check token price is $1
-    expect((await poolPerformance.tokenPrice(poolLogicProxy.address)).toString()).to.equal(oneDollar.toString());
-    // Check tokenPriceAdjustForPerformance == $1
-    expect((await poolPerformance.tokenPriceAdjustedForPerformance(poolLogicProxy.address)).toString()).to.equal(
-      oneDollar.toString(),
-    );
+    expect((await poolLogicProxy.tokenPrice()).toString()).to.equal(oneDollar.toString());
   });
 });
