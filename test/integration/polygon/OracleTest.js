6753db09f4fe91c4c54947b11817e24d2ccc6f61
hochbrj <jhochbrueckner@gmail.com>
2021-10-29 10:24:10 +1100

    chore(Oracle.test): Code styling

--- test/integration/polygon/OracleTest.js
+++ test/integration/polygon/OracleTest.js
@@ -4,27 +4,27 @@ const { use, expect } = require("chai");
 
 use(chaiAlmost());
 
-const oracleAddress = "0xE749cD67F9e6727b868bB2480195ee301aA4BA33"
+const oracleAddress = "0xE749cD67F9e6727b868bB2480195ee301aA4BA33";
 
 describe("DHedgePoolPriceOracle", function () {
-  let dhedgePoolPriceOracle
-  let poolLogic
+  let dhedgePoolPriceOracle;
+  let poolLogic;
 
   beforeEach(async function () {
     const DhedgePoolPriceOracle = await ethers.getContractFactory("DHedgePoolPriceOracle");
-     dhedgePoolPriceOracle = await DhedgePoolPriceOracle.attach(oracleAddress);
-     const poolAddress = await dhedgePoolPriceOracle.poolAddress()
-     const PoolLogic = await ethers.getContractFactory("PoolLogic");
-     poolLogic = await PoolLogic.attach(poolAddress)
+    dhedgePoolPriceOracle = await DhedgePoolPriceOracle.attach(oracleAddress);
+    const poolAddress = await dhedgePoolPriceOracle.poolAddress();
+    const PoolLogic = await ethers.getContractFactory("PoolLogic");
+    poolLogic = await PoolLogic.attach(poolAddress);
   });
 
   // Checks id pool address is set
   it("price should almost be the same as token price", async () => {
-    const priceFromOracle = await dhedgePoolPriceOracle.getPrice()
-    console.log("price from oracle ",priceFromOracle.toString())
-    const tokenPrice = await poolLogic.tokenPrice()
-    console.log("token Price ", tokenPrice.toString())
-    
+    const priceFromOracle = await dhedgePoolPriceOracle.getPrice();
+    console.log("price from oracle ", priceFromOracle.toString());
+    const tokenPrice = await poolLogic.tokenPrice();
+    console.log("token Price ", tokenPrice.toString());
+
     //not differ more than 10%
     expect(priceFromOracle.gte(tokenPrice.mul(9).div(10))).to.be.true;
     expect(priceFromOracle.lte(tokenPrice.mul(11).div(10))).to.be.true;
