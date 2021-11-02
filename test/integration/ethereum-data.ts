e3d94031693c975c0025b79be8c066001e6c3cc3
Jake Mc <jake.mc@icloud.com>
2021-11-02 12:26:16 +1000

    chore(working-test): update synths

--- test/integration/ethereum-data.ts
+++ test/integration/ethereum-data.ts
@@ -65,7 +65,7 @@ export const assets = {
   susd: "0x57Ab1ec28D129707052df4dF418D58a2D46d5f51",
   seth: "0x5e74C9036fb86BD7eCdcb084a0673EFc32eA31cb",
   slink: "0xbBC455cb4F1B9e4bFC4B73970d360c8f032EfEE6",
-  snx: "0x97767D7D04Fd0dB0A1a2478DCd4BA85290556B48",
+  snx: "0x54f25546260C7539088982bcF4b7dC8EDEF19f21",
 };
 
 export const price_feeds = {
