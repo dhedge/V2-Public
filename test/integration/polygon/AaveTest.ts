673f9a71fb845bf17572cb38bd638f0455704ccd
Jake Mc <jake.mc@icloud.com>
2022-02-10 12:28:35 +1000

    bugfix(aave-borrowing-fix): try to reset block state between each test [run-polygon]

--- test/integration/polygon/AaveTest.ts
+++ test/integration/polygon/AaveTest.ts
@@ -29,12 +29,7 @@ describe("Aave Test", function () {
   const iLendingPool = new ethers.utils.Interface(ILendingPool__factory.abi);
   let deployments: IDeployments;
 
-  let snapshot: any;
-  after(async () => {
-    await ethers.provider.send("evm_revert", [snapshot]);
-  });
   before(async function () {
-    snapshot = await ethers.provider.send("evm_snapshot", []);
     [logicOwner, manager, dao, user] = await ethers.getSigners();
 
     deployments = await deployPolygonContracts();
@@ -47,7 +42,13 @@ describe("Aave Test", function () {
     await getAccountToken(units(10000, 6), logicOwner.address, assets.usdc, assetsBalanceOfSlot.usdc);
   });
 
+  let snapshot: any;
+  afterEach(async () => {
+    await ethers.provider.send("evm_revert", [snapshot]);
+  });
+
   beforeEach(async function () {
+    snapshot = await ethers.provider.send("evm_snapshot", []);
     const funds = await createFund(poolFactory, logicOwner, manager, [
       { asset: assets.usdc, isDeposit: true },
       { asset: assets.weth, isDeposit: true },
