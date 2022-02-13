8c87a72217f22ecbe06d6d1f2dc4a4180b13e22b
Ermin <ermin@dhedge.org>
2022-02-13 16:34:43 +0800

    test(UniswapV3SwapRouterTest): Create integration test
    Swap router testing scope between unit and integration to be confirmed

---
+++ test/integration/utils/getMinAmountOut.ts
@@ -0,0 +1,33 @@
+import { ethers } from "hardhat";
+import { BigNumber } from "ethers";
+
+import { Deployments } from "../utils/deployContracts";
+import { units } from "../../TestHelpers";
+
+/**
+ * Gets a minimum amount out for swaps to pass Chainlink slippage checker
+ * @param deployments
+ * @param amountIn
+ * @param tokenInAddress
+ * @param tokenOutAddress
+ * @returns
+ */
+export const getMinAmountOut = async (
+  deployments: Deployments,
+  amountIn: BigNumber,
+  tokenInAddress: string,
+  tokenOutAddress: string,
+): Promise<BigNumber> => {
+  const tokenIn = await ethers.getContractAt("IERC20Extended", tokenInAddress);
+  const tokenOut = await ethers.getContractAt("IERC20Extended", tokenOutAddress);
+  const tokenInPrice = await deployments.assetHandler.getUSDPrice(tokenIn.address);
+  const tokenOutPrice = await deployments.assetHandler.getUSDPrice(tokenOut.address);
+
+  const tokenInDecimals = await tokenIn.decimals();
+  const tokenOutDecimals = await tokenOut.decimals();
+
+  const exchangeRate = tokenInPrice.mul(units(1, tokenOutDecimals)).div(tokenOutPrice);
+  const minAmountOut = amountIn.mul(exchangeRate).div(units(1, tokenInDecimals)).mul(91).div(100); // 91% minimum amount out
+
+  return minAmountOut;
+};
