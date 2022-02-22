b871f8bc002fb0f6bb2cdf85c2f62ab08438573c
Ermin <ermin@dhedge.org>
2022-02-22 19:17:15 +0800

    test(UniswapV3AssetGuardTest): Remove EVM increase time
    Uni TWAP pricing will not be used, so should be able to price the LP immediately

--- test/integration/polygon/UniswapV3AssetGuardTest.ts
+++ test/integration/polygon/UniswapV3AssetGuardTest.ts
@@ -175,8 +175,6 @@ describe("UniswapV3AssetGuardTest", function () {
         tickUpper: tick + tickSpacing,
       };
       await mintLpAsUser(nonfungiblePositionManager, user, mintSettings);
-      await ethers.provider.send("evm_increaseTime", [60 * 3]); // 3 minutes due to TWAP on pricing. TODO: remove if we decide to not use the TWAP
-      await ethers.provider.send("evm_mine", []);
 
       // Act
       const tokenPriceBefore = await poolLogicProxy.tokenPrice();
@@ -217,8 +215,6 @@ describe("UniswapV3AssetGuardTest", function () {
         tickUpper: currentTick + tickSpacing,
       };
       await mintLpAsUser(nonfungiblePositionManager, user, mintSettings);
-      await ethers.provider.send("evm_increaseTime", [60 * 3]); // 3 minutes due to TWAP on pricing. TODO: remove if we decide to not use the TWAP
-      await ethers.provider.send("evm_mine", []);
 
       // Act
       const tokenPriceBefore = await poolLogicProxy.tokenPrice();
