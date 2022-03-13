d22042d1503bc6ad22583f79fad154a72e2ebcbe
Jake Mc <jake.mc@icloud.com>
2022-03-13 19:55:49 +1000

    chore(refactor-asset-aggregators): refactor remove comments

--- deployment-scripts/upgrade/jobs/assetsJob.ts
+++ deployment-scripts/upgrade/jobs/assetsJob.ts
@@ -27,57 +27,55 @@ export const assetsJob: IJob<void> = async (
 
   const jsonAssets: TAssetConfig[] = await csv().fromFile(fileName);
 
-  fs.writeFileSync(fileName.replace(".csv", ".json"), JSON.stringify(jsonAssets, null, 2));
+  // Check for any accidental duplicate addresses or price feeds in the CSV
+  if (hasDuplicates(jsonAssets, (x) => x.assetAddress)) throw "Duplicate 'Address' field found in assets CSV";
 
-  // // Check for any accidental duplicate addresses or price feeds in the CSV
-  // if (hasDuplicates(jsonAssets, (x) => x.assetAddress)) throw "Duplicate 'Address' field found in assets CSV";
+  for (const jsonAsset of [...jsonAssets]) {
+    const foundInVersions = versions[config.newTag].contracts.Assets?.some((deployedAsset) => {
+      // We remove the deployed oracle address and then check all other fields are the same
+      // JSON does need to be ordered for this to work, so might need to use node-hasher here
+      const { oracleAddress, ...allOtherProps } = deployedAsset;
+      JSON.stringify(allOtherProps) == JSON.stringify(jsonAsset);
+    });
 
-  // for (const jsonAsset of [...jsonAssets]) {
-  //   const foundInVersions = versions[config.newTag].contracts.Assets?.some((deployedAsset) => {
-  //     // We remove the deployed oracle address and then check all other fields are the same
-  //     // JSON does need to be ordered for this to work, so might need to use node-hasher here
-  //     const { oracleAddress, ...allOtherProps } = deployedAsset;
-  //     JSON.stringify(allOtherProps) == JSON.stringify(jsonAsset);
-  //   });
+    if (!foundInVersions) {
+      console.log("Will Deploy Asset:", jsonAsset);
+      if (config.execute) {
+        const oracle = await getOracle(hre, jsonAsset);
+        newAssets.push(oracle);
+      }
+    }
+  }
 
-  //   if (!foundInVersions) {
-  //     console.log("Will Deploy Asset:", jsonAsset);
-  //     if (config.execute) {
-  //       const oracle = await getOracle(hre, jsonAsset);
-  //       newAssets.push(oracle);
-  //     }
-  //   }
-  // }
+  const AssetHandlerLogic = await hre.artifacts.readArtifact("AssetHandler");
+  const assetHandlerLogic = new ethers.utils.Interface(AssetHandlerLogic.abi);
+  // We need to convert them into the
+  const assetHanderAssets: { asset: string; assetType: number; aggregator: string }[] = newAssets.map((x) => {
+    return {
+      asset: x.assetAddress,
+      assetType: x.assetType,
+      aggregator: x.oracleAddress,
+    };
+  });
 
-  // const AssetHandlerLogic = await hre.artifacts.readArtifact("AssetHandler");
-  // const assetHandlerLogic = new ethers.utils.Interface(AssetHandlerLogic.abi);
-  // // We need to convert them into the
-  // const assetHanderAssets: { asset: string; assetType: number; aggregator: string }[] = newAssets.map((x) => {
-  //   return {
-  //     asset: x.assetAddress,
-  //     assetType: x.assetType,
-  //     aggregator: x.oracleAddress,
-  //   };
-  // });
+  const addAssetsABI = assetHandlerLogic.encodeFunctionData("addAssets", [assetHanderAssets]);
 
-  // const addAssetsABI = assetHandlerLogic.encodeFunctionData("addAssets", [assetHanderAssets]);
+  if (newAssets.length > 0) {
+    console.log("AssetsJob: Proposing New Assets");
+    await proposeTx(
+      versions[config.oldTag].contracts.AssetHandlerProxy,
+      addAssetsABI,
+      "Update assets in Asset Handler",
+      config,
+      addresses,
+    );
 
-  // if (newAssets.length > 0) {
-  //   console.log("AssetsJob: Proposing New Assets");
-  //   await proposeTx(
-  //     versions[config.oldTag].contracts.AssetHandlerProxy,
-  //     addAssetsABI,
-  //     "Update assets in Asset Handler",
-  //     config,
-  //     addresses,
-  //   );
-
-  //   const assetsWithNewOracles = new Set<string>(newAssets.map((x) => x.assetAddress));
-  //   // Filter out any assets that have a new oracle
-  //   const existingAssets = (versions[config.newTag].contracts.Assets || []).filter(
-  //     (existingAsset) => !assetsWithNewOracles.has(existingAsset.assetAddress),
-  //   );
-  //   console.log("New Assets", newAssets);
-  //   versions[config.newTag].contracts.Assets = [...existingAssets, ...newAssets];
-  // }
+    const assetsWithNewOracles = new Set<string>(newAssets.map((x) => x.assetAddress));
+    // Filter out any assets that have a new oracle
+    const existingAssets = (versions[config.newTag].contracts.Assets || []).filter(
+      (existingAsset) => !assetsWithNewOracles.has(existingAsset.assetAddress),
+    );
+    console.log("New Assets", newAssets);
+    versions[config.newTag].contracts.Assets = [...existingAssets, ...newAssets];
+  }
 };
