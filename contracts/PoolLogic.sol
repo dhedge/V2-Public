1089982b770615f943eae642fb21459ed98f654e
Jake Mc <jake.mc@icloud.com>
2022-04-19 14:10:48 +1000

    chore(streaming-fee-fixes): dont fix summary now

--- contracts/PoolLogic.sol
+++ contracts/PoolLogic.sol
@@ -561,9 +561,7 @@ contract PoolLogic is ERC20Upgradeable, ReentrancyGuardUpgradeable {
       uint256
     )
   {
-    (uint256 performanceFeeNumerator, uint256 managerFeeNumerator, uint256 managerFeeDenominator) = IPoolManagerLogic(
-      poolManagerLogic
-    ).getFee();
+    (uint256 performanceFeeNumerator, , uint256 managerFeeDenominator) = IPoolManagerLogic(poolManagerLogic).getFee();
     (uint256 exitFeeNumerator, uint256 exitFeeDenominator) = IHasFeeInfo(factory).getExitFee();
 
     return (
@@ -577,8 +575,7 @@ contract PoolLogic is ERC20Upgradeable, ReentrancyGuardUpgradeable {
       performanceFeeNumerator,
       managerFeeDenominator,
       exitFeeNumerator,
-      exitFeeDenominator,
-      managerFeeNumerator
+      exitFeeDenominator
     );
   }
 
